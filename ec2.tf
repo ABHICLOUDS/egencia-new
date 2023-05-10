7d23a4b9a087641c96ec17de11a2ffcff58c3421
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-10 14:22:36 +0530

    commit-3

--- ec2.tf
+++ ec2.tf
@@ -31,3 +31,52 @@ resource "aws_instance" "example_instance-2" {
     volume_type = "gp2"
   }
 }
+
+
+resource "aws_security_group" "example_sg-1" {
+  name_prefix = "${var.tags}-pl-sg-tf"
+  vpc_id      = aws_vpc.example_vpc.id
+  dynamic "ingress" {
+    for_each = [22, 80, 8080, 443]
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.tags}-pl-sg-tf"
+  }
+}
+
+resource "aws_security_group" "example_sg-2" {
+  name_prefix = "${var.tags}-il-sg-tf"
+  vpc_id      = aws_vpc.example_vpc.id
+  dynamic "ingress" {
+    for_each = [22, 80, 8080, 443]
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.tags}-il-sg-tf"
+  }
+}

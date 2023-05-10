ac4a83a0c50eedc144c9bb4d42e07efc0ace0b56
EC2 Default User <ec2-user@ip-172-31-31-98.ec2.internal>
2023-05-10 08:54:55 +0000

    commit-3

--- ec2.tf
+++ ec2.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "example_instance-1" {
 }
 
 resource "aws_instance" "example_instance-2" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = aws_subnet.private_subnets[0].id
-  vpc_security_group_ids      = [aws_security_group.example_sg-2.id]
-  iam_instance_profile        = var.instance_profile_name
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.private_subnets[0].id
+  vpc_security_group_ids = [aws_security_group.example_sg-2.id]
+  iam_instance_profile   = var.instance_profile_name
   tags = {
     Name = "${var.tags}-il-instance-tf"
   }

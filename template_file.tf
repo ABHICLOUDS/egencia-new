98e5992b95cf3087f4b0cc6bf3231a2d90199e57
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-23 15:07:57 +0530

    commit-15

--- template_file.tf
+++ template_file.tf
@@ -1,22 +1,5 @@
-data "aws_s3_object" "this" {
+data "aws_s3_object" "user_data_script" {
   bucket = "demo-8795"
-  key    = "apache-tomcat-7.0.64-PL.tar"
+  key    = "script.sh"
 }
 
-data "aws_s3_object" "war_file" {
-  bucket = "demo-8795"
-  key    = "egencia.war"
-}
-
-
-data "template_file" "user_data" {
-  template = <<-EOF
-    #!/bin/bash
-    aws s3 cp s3://${data.aws_s3_object.this.bucket}/${data.aws_s3_object.this.key} /usr/local
-    tar -xvf /usr/local/apache-tomcat-7.0.64-PL.tar -C /usr/local
-    aws s3 cp s3://${data.aws_s3_object.war_file.bucket}/${data.aws_s3_object.war_file.key} /usr/local/apache-tomcat-7.0.64-PL/webapps/
-    chown -R ec2-user:ec2-user /usr/local/apache-tomcat-7.0.64-PL/webapps/egencia.war
-    chmod +x /usr/local/apache-tomcat-7.0.64-PL/webapps/egencia.war
-    /usr/local/apache-tomcat-7.0.64-PL/bin/startup.sh
-  EOF
-}

abb1fd52468bd265b8115832094fd895bcac584d
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-24 17:19:07 +0530

    commit-40

---
+++ env/qa/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+module "vpc" {
+  source            = "../modules/vpc"
+  tags = var.tags
+  vpc_cidr_block    = var.vpc_cidr_block
+  public_subnet_cidr_blocks=var.public_subnet_cidr_blocks
+  public_subnet_azs=var.public_subnet_azs
+  private_subnet_cidr_blocks=var.private_subnet_cidr_blocks
+  private_subnet_azs=var.private_subnet_azs
+}
---
+++ env/qa/var.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr_block" {
+type=string
+}
+
+variable "aws_region" {
+type=string
+}
+
+variable "public_subnet_cidr_blocks" {
+  type = list(string)
+}
+
+variable "private_subnet_cidr_blocks" {
+  type = list(string)
+}
+
+variable "public_subnet_azs" {
+  type = list(string)
+}
+
+variable "private_subnet_azs" {
+  type = list(string)
+}

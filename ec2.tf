a63adf81e787d3b22185bb5fae007a832937b345
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-24 15:13:30 +0530

    commit-19

--- ec2.tf
+++ ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "example_instances" {
   iam_instance_profile        = var.instance_profile_name
   user_data                    = data.aws_s3_object.user_data_script.body
   tags = {
-    Name      = "${var.tags}-pl-instance${count.index + 1}-tf-${aws_instance.example.private_ip}"
+    Name      = "${var.tags}-pl-instance${count.index + 1}-tf-${aws_instance.example_instances.private_ip}"
   }
   root_block_device {
     volume_size = var.ebs_volume

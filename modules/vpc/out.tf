ab6de78374cace16b0241b305a87474c80870075
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-25 12:34:22 +0530

    commit-50

---
+++ modules/vpc/out.tf
@@ -0,0 +1,11 @@
+output "public-subnet" {
+  value=aws_subnet" "public_subnets[*].id
+}
+
+output "private-subnet" {
+  value=aws_subnet" "private_subnets[*].id
+}
+
+output "vpc_id" {
+  value=.aws_vpc.this_vpc.id
+}

5f9478b909167f023f77417cde57a70045d389e3
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-26 17:21:13 +0530

    commit

--- env/qa/main.tf
+++ env/qa/main.tf
@@ -23,46 +23,46 @@ module "vpc" {
   private_subnet_azs          = var.private_subnet_azs
 }
 
-module "ec-2" {
-  source                      = "../../modules/ec-2"
-  vpc_id                      = module.vpc.vpc_id
-  bucket_name                 = var.bucket_name
-  bucket_pl_script            = var.bucket_pl_script
-  pl_count                    = var.pl_count
-  il_count                    = var.il_count
-  ami_id                      = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  ebs_volume                  = var.ebs_volume
-  ebs_volume_type             = var.ebs_volume_type
-  instance_profile_name       = var.instance_profile_name
-  sg_port                     = var.sg_port
-  appname                     = var.appname
-  env                         = var.env
-  tags                        = var.tags
-  public_subnet_ids           = module.vpc.public_subnet_ids
-  private_subnet_ids          = module.vpc.private_subnet_ids
+module "ec2" {
+  source                     = "../../modules/ec2"
+  vpc_id                     = module.vpc.vpc_id
+  bucket_name                = var.bucket_name
+  bucket_pl_script           = var.bucket_pl_script
+  pl_count                   = var.pl_count
+  il_count                   = var.il_count
+  ami_id                     = var.ami_id
+  instance_type              = var.instance_type
+  key_name                   = var.key_name
+  ebs_volume                 = var.ebs_volume
+  ebs_volume_type            = var.ebs_volume_type
+  instance_profile_name      = var.instance_profile_name
+  sg_port                    = var.sg_port
+  appname                    = var.appname
+  env                        = var.env
+  tags                       = var.tags
+  public_subnet_ids          = module.vpc.public_subnet_ids
+  private_subnet_ids         = module.vpc.private_subnet_ids
 }
 
 # Internet-facing (PL) load balancer
 module "pl_lb" {
   source = "../../modules/alb"
 
-  alb_name               = "${var.tags["Name"]}-example-lb"
+  alb_name               = "${var.tags["Name"]}-example-pl-lb"
   internal               = false
   load_balancer_type     = "application"
   security_groups        = [aws_security_group.example.id]
-  subnets                = var.public_subnets
+  subnets                = module.vpc.public_subnet_ids
 
-  target_group_name      = "${var.tags["Name"]}-example-tg"
+  target_group_name      = "${var.tags["Name"]}-example-pl-tg"
   target_group_port      = var.tg_port
   target_group_protocol  = var.tg_protocol
-  vpc_id                 = aws_vpc.this_vpc.id
+  vpc_id                 = module.vpc.vpc_id
   health_check_path      = var.pl_hc_path
   listener_port          = var.pl_listener_port
   listener_protocol      = var.pl_listener_protocol
   target_count           = var.pl_count
-  target_ids             = module.ec-2.pl_instance_ids
+  target_ids             = module.ec2.pl_instance_ids
   target_port            = var.pl_tg_attach_port
 }
 
@@ -70,20 +70,21 @@ module "pl_lb" {
 module "il_lb" {
   source = "../../modules/alb"
 
-  alb_name               = "${var.tags["Name"]}-example-ilb"
+  alb_name               = "${var.tags["Name"]}-example-il-lb"
   internal               = true
   load_balancer_type     = "application"
   security_groups        = [aws_security_group.example.id]
-  subnets                = var.private_subnets
+  subnets                = module.vpc.private_subnet_ids
 
-  target_group_name      = "${var.tags["Name"]}-example-itg"
+  target_group_name      = "${var.tags["Name"]}-example-il-tg"
   target_group_port      = var.tg_port
   target_group_protocol  = var.tg_protocol
-  vpc_id                 = aws_vpc.this_vpc.id
+  vpc_id                 = module.vpc.vpc_id
   health_check_path      = var.il_hc_path
   listener_port          = var.il_listener_port
   listener_protocol      = var.il_listener_protocol
   target_count           = var.il_count
-  target_ids             = module.ec-2.il_instance_ids
+  target_ids             = module.ec2.il_instance_ids
   target_port            = var.il_tg_attach_port
 }
+

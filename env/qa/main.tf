707eb655b4b7b9c9c5bd006ad1d4dd4d255ce38c
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-26 19:48:41 +0530

    commit

--- env/qa/main.tf
+++ env/qa/main.tf
@@ -36,30 +36,56 @@ module "vpc" {
   }
 }
 
-module "ec2" {
-  source  = "../../modules/ec-2"
+# main.tf
+
+module "vpc" {
+  source  = "./vpc"
   # Pass module-specific variables here
-  bucket_name            = "example-bucket"
-  bucket_pl_script       = "example-script.sh"
-  pl_count               = 2
-  il_count               = 2
-  instance_type          = "t2.micro"
-  ami_id                 = "ami-12345678"
-  key_name               = "example-key"
-  instance_profile_name  = "example-profile"
-  ebs_volume             = 50
-  ebs_volume_type        = "gp2"
+  vpc_cidr_block             = "10.0.0.0/16"
+  public_subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidr_blocks = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones         = ["us-east-1a", "us-east-1b"]
+  security_group_name        = "example-security-group"
+  security_group_description = "Example security group"
+  security_group_ingress_from_port   = 22
+  security_group_ingress_to_port     = 22
+  security_group_ingress_protocol    = "tcp"
+  security_group_ingress_cidr_blocks = ["0.0.0.0/0"]
+  security_group_egress_from_port    = 0
+  security_group_egress_to_port      = 0
+  tags = {
+    Name        = "Example VPC"
+    Environment = "Production"
+  }
+}
+
+module "ec2" {
+  source  = "./ec2"
+  # Pass module-specific variables here
+  appname                  = "example-app"
+  env                      = "production"
+  bucket_name              = "example-bucket"
+  bucket_pl_script         = "example-script.sh"
+  sg_port                  = [80, 443]
+  pl_count                 = 2
+  il_count                 = 2
+  instance_type            = "t2.micro"
+  ami_id                   = "ami-12345678"
+  key_name                 = "example-key"
+  instance_profile_name    = "example-profile"
+  ebs_volume               = 50
+  ebs_volume_type          = "gp2"
   tags = {
     Name        = "Example EC2"
     Environment = "Production"
   }
-  public_subnet_ids      = module.vpc.public_subnet_ids
-  private_subnet_ids     = module.vpc.private_subnet_ids
-  vpc_id                 = module.vpc.vpc_id
+  public_subnet_ids        = module.vpc.public_subnet_ids
+  private_subnet_ids       = module.vpc.private_subnet_ids
+  vpc_id                   = module.vpc.vpc_id
 }
 
 module "pl_alb" {
-  source  = "../../modules/alb"
+  source  = "./alb"
   # Pass module-specific variables here
   alb_name               = "pl-example-alb"
   internal               = false
@@ -83,7 +109,7 @@ module "pl_alb" {
 }
 
 module "il_alb" {
-  source  = "../../modules/alb"
+  source  = "./alb"
   # Pass module-specific variables here
   alb_name               = "il-example-alb"
   internal               = true
@@ -91,7 +117,7 @@ module "il_alb" {
   security_groups        = [module.vpc.security_group_id]
   subnets                = module.vpc.private_subnet_ids
   target_group_name      = "il-example-target-group"
-  target_group_port      = 8080
+  target_group_port      = 80
   target_group_protocol  = "HTTP"
   vpc_id                 = module.vpc.vpc_id
   health_check_path      = "/"

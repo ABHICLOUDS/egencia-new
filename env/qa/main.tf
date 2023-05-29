d01eb063a536928e396660c49db5702adde84bd3
Abhishek Prasad1 <Abhishek.Prasad1@igtsolutions.com>
2023-05-29 16:20:16 +0530

    commit

--- env/qa/main.tf
+++ env/qa/main.tf
@@ -1,4 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket         = "egencia-demo-s3-tf-state"
+    key            = "egencia/terraform.tfstate/"
+    region         = "us-east-1"
+    dynamodb_table = "egencia-demo-db"
+    encrypt        = true
+  }
+}
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
